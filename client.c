520e72e0ebe8ae82d7c4bf2a99ca568e40c961ee
João Antão <jp.antao@campus.fct.unl.pt>
2023-01-19 14:45:38 +0000

    base client operation

--- client.c
+++ client.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <libmemcached/memcached.h>
 
 
 #define DEFAULT_CONFIG "--SERVER=127.0.0.1:11211"
-#define DEFAULT_N_KEYS 1000000 // default number of items to insert = 1 M
-#define DEFAULT_KEY_LEN 16 // default key size = 16 bytes
-#define DEFAULT_VAL_LEN 1024 // default value size = 1 K
-#define DEFAULT_N_OPS 1000000 // default number of operations to perform = 1 M
+#define DEFAULT_N_KEYS 10       // default number of items to insert
+#define DEFAULT_KEY_LEN 16      // default key size = 16 bytes
+#define DEFAULT_VAL_LEN 1024    // default value size = 1 K
+#define DEFAULT_N_OPS 10        // default number of operations to perform
 
 char *config_string = DEFAULT_CONFIG;
 int n_keys = DEFAULT_N_KEYS;
@@ -27,15 +28,14 @@ static __inline__ unsigned long time_diff(struct timeval *start, struct timeval 
 int generate_string(char* str, int len) {
     int i;
     for (i = 0; i < len; i++) {
-        ptr[i] = 'a' + (rand() % 26);
+        str[i] = 'a' + (rand() % 26);
     }
     str[len] = '\0';
     return 0;
 }
 
 int init_keys(char **keys, int n, int len) {
-    int i;
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         keys[i] = (char *) malloc(len + 1);
         generate_string(keys[i], len);
     }
@@ -56,13 +56,15 @@ int main(int argc, char *argv[]) {
 
     struct timeval tstart, tend;
 
-    char **keys = malloc(n_keys);
+
+    char **keys = malloc(n_keys * sizeof(char *));
     init_keys(keys, n_keys, key_len);
 
     // insert keys
     for (int i = 0; i < n_keys; i++) {
         char value[val_len];
         generate_string(value, val_len);
+        printf("Inserting key %s with value %s\n", keys[i], value);
         memcached_add(memc, keys[i], key_len, value , val_len,0, 0);
     }
 
@@ -72,15 +74,20 @@ int main(int argc, char *argv[]) {
         char* key = keys[i % n_keys];
 
         // get key
+        printf("Getting key %s\n", key);
         size_t value_length;
         uint32_t flags;
         memcached_return rc;
         char *value = memcached_get(memc, key, key_len, &value_length, &flags, &rc);
-
-        // generate new value (required? or can we use a predefined one?)
-        generate_string(value, val_len);
+        if (value == NULL) {
+            printf("memcached_get() failed: %s, key: %s\n", memcached_strerror(memc, rc), key);
+            exit(1);
+        }
 
         // update key
+        // generate new value (required? or can we use a predefined one?)
+        generate_string(value, val_len);
+        printf("Updating key %s with value %s\n", key, value);
         memcached_replace(memc, key, key_len, value, val_len, 0, 0);
         free(value);
     }
